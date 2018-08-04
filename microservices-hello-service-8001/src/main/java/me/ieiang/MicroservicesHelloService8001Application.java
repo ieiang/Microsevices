420af50a6031343c9805d3f33c2bba402458e036
ieiang <ieiang@163.com>
2018-08-04 23:02:55 +0800

    新增zipkin+hystrix dashboard

--- microservices-hello-service-8001/src/main/java/me/ieiang/MicroservicesHelloService8001Application.java
+++ microservices-hello-service-8001/src/main/java/me/ieiang/MicroservicesHelloService8001Application.java
@@ -1,9 +1,11 @@
 package me.ieiang;
 
+import brave.sampler.Sampler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +25,10 @@ public class MicroservicesHelloService8001Application {
     public String hi(@RequestParam String name) {
         return "hi " + name + ",i am from port:" + port;
     }
+
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
 }
--- microservices-ribbon-consumer/src/main/java/me/ieiang/MicroservicesRibbonConsumerApplication.java
+++ microservices-ribbon-consumer/src/main/java/me/ieiang/MicroservicesRibbonConsumerApplication.java
@@ -1,16 +1,22 @@
 package me.ieiang;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
+@EnableCircuitBreaker
+//@EnableTurbine
 public class MicroservicesRibbonConsumerApplication {
 
     public static void main(String[] args) {
@@ -23,7 +29,10 @@ public class MicroservicesRibbonConsumerApplication {
         return new RestTemplate();
     }
 
-
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
+    }
 
 
 
