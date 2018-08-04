b2ff27e29b7ad09cee8cffb4c3c7f2cfd8aafc0b
ieiang <ieiang@163.com>
2018-08-04 18:44:48 +0800

    新增spring-bus

--- microservices-config-client/src/main/java/me/ieiang/MicroservicesConfigClientApplication.java
+++ microservices-config-client/src/main/java/me/ieiang/MicroservicesConfigClientApplication.java
@@ -3,10 +3,14 @@ package me.ieiang;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableEurekaClient
+@RefreshScope
 @SpringBootApplication
 public class MicroservicesConfigClientApplication {
 
