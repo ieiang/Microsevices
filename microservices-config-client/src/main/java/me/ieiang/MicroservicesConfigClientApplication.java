2540f6aed0d2a621463849051ee2651509646512
ieiang <ieiang@163.com>
2018-08-04 17:26:44 +0800

    新增spring-config

--- microservices-config-client/src/main/java/me/ieiang/MicroservicesConfigClientApplication.java
+++ microservices-config-client/src/main/java/me/ieiang/MicroservicesConfigClientApplication.java
@@ -1,13 +1,24 @@
 package me.ieiang;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 @SpringBootApplication
 public class MicroservicesConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroservicesConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @GetMapping("/hi")
+    public String hi() {
+        return foo;
     }
 }
